16e99dfb68188ee2f0a2164adb5c7630f671c8b5
kubaracek <me@jakubracek.net>
2021-02-23 19:03:52 +0100

    Charge gateway method

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,26 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.core.exceptions.UnbillableInvoice
+import io.pleo.antaeus.core.exceptions.NotEnoughBalance
+import io.pleo.antaeus.models.Charge
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 
 class BillingService(
     private val paymentProvider: PaymentProvider
 ) {
-// TODO - Add code e.g. here
+
+    // Make an actual call to the gateway
+    private fun chargeGateway(invoice: Invoice): Charge =
+        runCatching {
+            paymentProvider.charge(invoice)
+        }.fold(
+            { result -> when(result) {
+                  true -> Charge.Successful(null)
+                  false -> Charge.Unsuccessful(null, NotEnoughBalance(invoice.id), null)
+              }
+            },
+            {Charge.Unsuccessful(null, it, null)}
+        )
 }
